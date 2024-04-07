6d082dacf6203139ad6e583269b84fb05c35424c
simonf2000 <simonf2000@outlook.com>
2024-04-07 11:16:19 +0800

    声音删除-腾讯点播平台音频文件删除

--- service/service-album/src/main/java/com/atguigu/tingshu/album/service/impl/VodServiceImpl.java
+++ service/service-album/src/main/java/com/atguigu/tingshu/album/service/impl/VodServiceImpl.java
@@ -6,13 +6,17 @@ import com.atguigu.tingshu.vo.album.TrackMediaInfoVo;
 import com.tencentcloudapi.common.AbstractModel;
 import com.tencentcloudapi.common.Credential;
 import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
 import com.tencentcloudapi.vod.v20180717.VodClient;
 import com.tencentcloudapi.vod.v20180717.models.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@Slf4j
 public class VodServiceImpl implements VodService {
 
     @Autowired
@@ -22,7 +26,7 @@ public class VodServiceImpl implements VodService {
     @Override
     public TrackMediaInfoVo getMediaInfo(String mediaFileId) {
         TrackMediaInfoVo trackMediaInfoVo = new TrackMediaInfoVo();
-        try{
+        try {
             // 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
             // 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
             // 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
@@ -67,6 +71,18 @@ public class VodServiceImpl implements VodService {
 
     @Override
     public void deleteMedia(String mediaFileId) {
-
+        try {
+            // 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
+            Credential cred = new Credential(vodConstantProperties.getSecretId(), vodConstantProperties.getSecretKey());
+            // 实例化要请求产品的client对象,clientProfile是可选的
+            VodClient client = new VodClient(cred, "");
+            // 实例化一个请求对象,每个接口都会对应一个request对象
+            DeleteMediaRequest req = new DeleteMediaRequest();
+            req.setFileId(mediaFileId);
+            // 返回的resp是一个DeleteMediaResponse的实例，与请求对象对应
+            client.DeleteMedia(req);
+        } catch (Exception e) {
+            log.error("[专辑服务]删除点播平台文件异常:", e);
+        }
     }
 }

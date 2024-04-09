568d2b3584f69e9a4855548c7c6ff25394ca0727
simonf2000 <simonf2000@outlook.com>
2024-04-09 20:24:00 +0800

    索引库文档上架-用户服务Feign接口-查询用户信息

--- service-client/service-user-client/src/main/java/com/atguigu/tingshu/user/client/impl/UserDegradeFeignClient.java
+++ service-client/service-user-client/src/main/java/com/atguigu/tingshu/user/client/impl/UserDegradeFeignClient.java
@@ -1,10 +1,19 @@
 package com.atguigu.tingshu.user.client.impl;
 
 
+import com.atguigu.tingshu.common.result.Result;
 import com.atguigu.tingshu.user.client.UserFeignClient;
+import com.atguigu.tingshu.vo.user.UserInfoVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class UserDegradeFeignClient implements UserFeignClient {
 
+    @Override
+    public Result<UserInfoVo> getUserInfoVo(Long userId) {
+        log.error("[用户服务]远程调用用户服务getUserInfoVo服务降级");
+        return null;
+    }
 }
